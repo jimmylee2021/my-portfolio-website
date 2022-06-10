bd4ef8d94182a0b376ef3cab34d03d895ea35fa3
Peter Adejimi <peteritadejimi@gmail.com>
2022-06-10 17:47:04 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector('.nav');
+
+menu.addEventListener('click',  () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+} )
