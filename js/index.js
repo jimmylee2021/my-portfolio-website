9ef78630d40d16f594861a3ad404c548b265853c
Peter Adejimi <peteritadejimi@gmail.com>
2022-07-01 15:38:20 +0100

    commit

--- js/index.js
+++ js/index.js
@@ -9,4 +9,11 @@ let navbar = document.querySelector('.nav');
 menu.addEventListener('click',  () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
+
+
+    window.onscroll = () => {
+        menu.classList.remove('fa-times');
+        navbar.classList.remove('active');
+
+    }
 } )
